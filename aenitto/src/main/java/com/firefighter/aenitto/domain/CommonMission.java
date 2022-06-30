da7d912e7abcd78c168cb76d3ecf66b0e59db404
LeoBang <bluke8489@gmail.com>
2022-07-01 00:58:27 +0900

    [FEAT] Mission 관련 Entity 생성 (#9) - Mission Entity 생성 - MissionType Enum (Common, Individual) 생성 - CommonMission, Individual Mission 생성 	- date 필드는 나중에 스케줄링 시 전날에 일괄 생성할 수도 있어서 자동생성 하지 않음.

---
+++ aenitto/src/main/java/com/firefighter/aenitto/domain/CommonMission.java
@@ -0,0 +1,29 @@
+package com.firefighter.aenitto.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommonMission {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "common_mission_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+
+    private LocalDate date;
+
+    @Builder
+    public CommonMission(LocalDate date) {
+        this.date = date;
+    }
+}
