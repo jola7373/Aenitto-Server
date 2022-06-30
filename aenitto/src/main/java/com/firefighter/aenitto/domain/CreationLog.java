821ed79be698bb679ac47caba47299f2edd82fd5
LeoBang <bluke8489@gmail.com>
2022-07-01 00:36:54 +0900

    [FEAT] Message Entity 생성 (#9) - created_at만 자동 주입해주는 CreationLog base entity 정의 - Message entity 정의

---
+++ aenitto/src/main/java/com/firefighter/aenitto/domain/CreationLog.java
@@ -0,0 +1,19 @@
+package com.firefighter.aenitto.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners({AuditingEntityListener.class})
+public class CreationLog {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
