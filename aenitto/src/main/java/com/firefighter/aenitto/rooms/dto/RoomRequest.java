cd147f24f4b870f29207095ac7bb31a4f5aca356
LeoBang <bluke8489@gmail.com>
2022-07-03 23:16:03 +0900

    [Feat] RoomController createRoom 구현 (#19) - RoomRequest Validation 추가

--- aenitto/src/main/java/com/firefighter/aenitto/rooms/dto/RoomRequest.java
+++ aenitto/src/main/java/com/firefighter/aenitto/rooms/dto/RoomRequest.java
@@ -5,12 +5,25 @@ import com.firefighter.aenitto.rooms.domain.Room;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
+@NoArgsConstructor(force = true)
 public class RoomRequest {
+    @NotNull
+    @Size(min = 1, max = 8)
     private final String title;
+    @NotNull
+    @Min(5) @Max(15)
     private final int capacity;
+    @NotNull
     private final String startDate;
+    @NotNull
     private final String endDate;
 
     public Room toEntity() {
