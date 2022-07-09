f9a3b41db6d407acc208118191c4af715785a1af
LeoBang <bluke8489@gmail.com>
2022-07-09 16:48:36 +0900

    [Feat] RoomController 초대코드 검증 구현 (#19)

--- src/main/java/com/firefighter/aenitto/rooms/controller/RoomController.java
+++ src/main/java/com/firefighter/aenitto/rooms/controller/RoomController.java
@@ -2,6 +2,8 @@ package com.firefighter.aenitto.rooms.controller;
 
 import com.firefighter.aenitto.members.domain.Member;
 import com.firefighter.aenitto.rooms.dto.request.CreateRoomRequest;
+import com.firefighter.aenitto.rooms.dto.request.VerifyInvitationRequest;
+import com.firefighter.aenitto.rooms.dto.response.VerifyInvitationResponse;
 import com.firefighter.aenitto.rooms.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,5 +31,21 @@ public class RoomController {
         final Member member = Member.builder().nickname("mock").build();
         final Long roomId = roomService.createRoom(member, createRoomRequest);
         return ResponseEntity.created(URI.create("/api/v1/rooms/" + roomId)).build();
+    }
+
+    @PostMapping("/invitations/verification")
+    public ResponseEntity verifyInvitation(
+            @Valid @RequestBody final VerifyInvitationRequest request
+    ) {
+        final Member member = mockLoginMember();
+        final VerifyInvitationResponse response = roomService.verifyInvitation(member, request);
+        return ResponseEntity.ok(response);
+    }
+
+    private Member mockLoginMember() {
+        return Member.builder()
+                .id(UUID.randomUUID())
+                .nickname("Mock")
+                .build();
     }
 }
