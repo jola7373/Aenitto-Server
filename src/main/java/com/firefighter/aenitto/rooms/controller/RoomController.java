aedc209b0f0f6ebac57404eea5d2e3f19abb6e63
LeoBang <bluke8489@gmail.com>
2022-07-09 23:40:45 +0900

    [Feat] RoomController 방 참여 구현 (#19)

--- src/main/java/com/firefighter/aenitto/rooms/controller/RoomController.java
+++ src/main/java/com/firefighter/aenitto/rooms/controller/RoomController.java
@@ -2,16 +2,14 @@ package com.firefighter.aenitto.rooms.controller;
 
 import com.firefighter.aenitto.members.domain.Member;
 import com.firefighter.aenitto.rooms.dto.request.CreateRoomRequest;
+import com.firefighter.aenitto.rooms.dto.request.ParticipateRoomRequest;
 import com.firefighter.aenitto.rooms.dto.request.VerifyInvitationRequest;
 import com.firefighter.aenitto.rooms.dto.response.VerifyInvitationResponse;
 import com.firefighter.aenitto.rooms.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -40,6 +38,16 @@ public class RoomController {
         final Member member = mockLoginMember();
         final VerifyInvitationResponse response = roomService.verifyInvitation(member, request);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/rooms/{roomId}/participants")
+    public ResponseEntity participateRoom(
+            @PathVariable final Long roomId,
+            @RequestBody final ParticipateRoomRequest request
+    ) {
+        final Member member = mockLoginMember();
+        roomService.participateRoom(member, roomId, request);
+        return ResponseEntity.created(URI.create("/api/v1/rooms/" + roomId)).build();
     }
 
     private Member mockLoginMember() {
