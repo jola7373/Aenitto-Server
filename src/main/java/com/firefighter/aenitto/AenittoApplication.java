f8609e635f55e15848fa1d7a5eaa36645ee90f52
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-07-14 13:01:21 +0900

    [Chore] add javax.xml.bind

--- src/main/java/com/firefighter/aenitto/AenittoApplication.java
+++ src/main/java/com/firefighter/aenitto/AenittoApplication.java
@@ -2,10 +2,12 @@ package com.firefighter.aenitto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AenittoApplication {
 
 	public static void main(String[] args) {
