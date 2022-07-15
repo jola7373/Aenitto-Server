b58c5810f0f22cc20c2fbf464a0ac431c37ab56a
LeoBang <bluke8489@gmail.com>
2022-07-15 12:09:01 +0900

    [Test] RequestDtoBuilder 추가 (#26)

--- src/test/java/com/firefighter/aenitto/rooms/dto/RoomRequestDtoBuilder.java
+++ src/test/java/com/firefighter/aenitto/rooms/dto/RoomRequestDtoBuilder.java
@@ -1,6 +1,8 @@
 package com.firefighter.aenitto.rooms.dto;
 
 import com.firefighter.aenitto.rooms.dto.request.CreateRoomRequest;
+import com.firefighter.aenitto.rooms.dto.request.ParticipateRoomRequest;
+import com.firefighter.aenitto.rooms.dto.request.VerifyInvitationRequest;
 
 public class RoomRequestDtoBuilder {
 
@@ -11,6 +13,18 @@ public class RoomRequestDtoBuilder {
                 .startDate("2022.06.20")
                 .endDate("2022.06.30")
                 .colorIdx(2)
+                .build();
+    }
+
+    public static VerifyInvitationRequest verifyInvitationRequest() {
+        return VerifyInvitationRequest.builder()
+                .invitationCode("5R2DV2")
+                .build();
+    }
+
+    public static ParticipateRoomRequest participateRoomRequest() {
+        return ParticipateRoomRequest.builder()
+                .colorIdx(1)
                 .build();
     }
 }
